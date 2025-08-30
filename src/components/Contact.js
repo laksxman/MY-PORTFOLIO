887eb9e448769d48933bd9e3c9da096d0841d799
Lakshman Kumar <kumarlaksxman17@gmail.com>
2025-08-30 22:51:03 +0530

    fully updated

---
+++ src/components/Contact.js
@@ -0,0 +1,68 @@
+import React, { useRef } from 'react';
+import './Contact.css'
+import Linkedin from '../assets/linkedin.svg'
+import Github from '../assets/github.svg'
+import Instagram from '../assets/instagram.svg'
+import Facebook from '../assets/facebook.svg'
+import emailjs from '@emailjs/browser';
+
+
+const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+
+        
+        e.preventDefault();
+        emailjs
+          .sendForm('service_pt0mwcp', 'template_zrh52jp', form.current, {
+            publicKey: 'b2kM4HMqa-VOZOn7U',
+          })
+          .then(
+                () => {
+                alert('Your Message has been submitted successfully!');
+                form.current.reset(); // form clear kar dega
+                },
+                (error) => {
+                alert('Failed to send the message. Please try again.');
+                console.log('FAILED...', error.text);
+                }
+            );
+      };
+
+
+
+return (
+    <section id = 'contact'>
+        <div id = 'social'>
+            
+            <h1 className='socialTitle'>Socials</h1>
+            <p className='socialDesc'>Here you can connect with me on my social media</p>
+          
+            <div className='socialImgs'>
+                <a href='https://www.linkedin.com/public-profile/settings'><img src = {Linkedin} alt = 'Social' className='socialImg'/></a>
+                <a href='https://github.com/laksxman/lakshman-kumar.git'><img src = {Github} alt = 'Social' className='socialImg'/></a> 
+                <a href='https://www.instagram.com/lakshman_yadav73?igsh=emJsZDdjcnIyd3c3'><img src = {Instagram} alt = 'Social' className='socialImg'/></a>
+                <a href='https://www.facebook.com/profile.php?id=100042128401304&mibextid=ZbWKwL'><img src = {Facebook} alt = 'Social' className='socialImg'/></a>
+            </div>
+        </div>
+
+        <div id = 'contactPage'>
+            
+            <h1 id = 'contactTitle'>Contact</h1>
+            <span className = 'contactDesc'>Feel free to contact me</span>
+
+            <form ref={form} onSubmit={sendEmail} className='contactForm'>
+                <input type='text' className='name' placeholder='Your Name' name='from_name' required />
+                <input type='email' className='name' placeholder='Your Email' name='your_email' required />
+                <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
+                <button type='submit' className='submitBtn'>Submit</button>
+            </form>
+
+            
+            
+        </div>
+    </section>
+)
+}
+
+export default Contact
---
+++ src/components/Project.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Project.css'
+import Airnb from '../assets/airnb.webp'
+import Zoom from '../assets/zoom2.png'
+import Zerodha from '../assets/zerodha.png'
+import Cafe from '../assets/cafe.webp'
+
+function Project() {
+  return (
+    <section id="project">
+        <h2 className='projectTitle'>My Projects</h2>
+        <span className='projectDesc'> Here are my Projects</span>
+
+        <div className='project-item'>
+            <div className='item-details'>
+                <p className='p1'>Airnb App</p>
+                <p className='p2'>TechStac:- React JS & node JS </p>
+            </div>
+            <a href='https://airnub-website.onrender.com'> <img src={Airnb} alt='Airnb App' className='projectImg'/> </a>
+        </div>
+
+        <div className='project-item'>
+            <div className='item-details'>
+                <p className='p1'>Zoom App</p>
+                <p className='p2'>TechStac: -React JS & Node JS</p>
+            </div>
+            <a href='https://zoomvideocallfrontend-jc9j.onrender.com'> <img src={Zoom} alt='Zoom App' className='projectImg'/> </a>
+        </div>
+
+        <div className='project-item'>
+            <div className='item-details'>
+                <p className='p1'>Zerodha App</p>
+                <p className='p2'>TechStac: -React JS & Node JS</p>
+            </div>
+            <a href='https://zerodhafrontend-0qv9.onrender.com'> <img src={Zerodha} alt='Zerodha App' className='projectImg'/> </a>
+        </div>
+
+        <div className='project-item'>
+            <div className='item-details'>
+                <p className='p1'>Cafe Website </p>
+                <p className='p2'>TechStac:- HTML, CSS and JavaScript</p>
+            </div>
+            <a href='https://coffeecorner-bdlr.onrender.com'> <img src={Cafe} alt='Cafe Website' className='projectImg'/> </a>
+        </div>
+    </section>
+  )
+}
+
+export default Project
