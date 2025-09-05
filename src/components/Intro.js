7513787d89ea4ad0ba28a9bd83bc2d72216fb420
Lakshman Kumar <kumarlaksxman17@gmail.com>
2025-09-05 14:50:18 +0530

    resume updated

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,33 +2,46 @@ import React from 'react'
 import './Intro.css'
 import Hire from '../assets/hireme.png'
 import reactLogo from '../assets/react.svg'
+import Resume from '../assets/resumeIcon.png';
 import { Link } from 'react-scroll'
 
 function Intro() {
+  const handleResumeDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/LakshmanKumar_Resume.pdf"; // File should be in public folder
+    link.download = "LakshmanKumar_Resume.pdf";
+    link.click();
+  };
+
   return (
     <section id="intro">
-        <div className="introContent">
-            <span className="hello" >Hello,</span>
-            <span className="introText">
-              I'm <span className="introName">Lakshman Kumar</span>
-              <br/>
-              Full Stack Web Developer
-            </span>
-            <p className="introPara" >
-              I am a skilled full stack web developer with exprinence in creating visually 
-              <br/>
-               appealing and user friendly websites.
-            </p>
-            <Link to="contactPage">
-            <button className='btn2'>
-                <img src={Hire} alt='Img' className="btnImg2"  />
-                Hire Me
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I'm <span className="introName">Lakshman Kumar</span>
+          <br />
+          Full Stack Web Developer
+        </span>
+        <p className="introPara">
+          I am a skilled full stack web developer with experience in creating visually 
+          <br />
+          appealing and user friendly websites.
+        </p>
+        <div className="resumeHire">
+          <button className="btn2" onClick={handleResumeDownload}>
+            <img src={Resume} alt="Img" className="btnImg2" />
+            Resume
+          </button>
+          <Link to="contactPage" smooth={true} duration={500}>
+            <button className="btn2">
+              <img src={Hire} alt="Img" className="btnImg2" />
+              Hire Me
             </button>
-            </Link>
+          </Link>
         </div>
+      </div>
 
-        <img src={reactLogo} alt='React Logo' className="logo2" />
-
+      <img src={reactLogo} alt="React Logo" className="logo2" />
     </section>
   )
 }
